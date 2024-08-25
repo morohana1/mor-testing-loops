d9c2615ebb5a05b8d75487d0fc768e1e78db20c3
morohana1 <morohana1801@gmail.com>
2024-08-25 20:45:57 +0300

    Add files via upload

---
+++ fullstack/javascript/index.js
@@ -0,0 +1,27 @@
+.//function fizzBuzz (to) {
+
+//let num = 15
+   // for(let Num=0; Num<= to; Num++)
+
+     if (Num%3 === 0 && Num%5 === 0) {
+        console.log ("fizzBuzz")
+    }
+    else if (Num%5 === 0) {
+        console.log ("Buzz")
+    }
+     else if (Num%3 === 0){
+        console.log ("fizz")
+    }
+
+    else {
+        console.log (Num)
+    }
+//}
+
+//fizzBuzz(100)
+
+while (returnCondition()) {
+    console.log("iterated...")
+}
+
+
